61492f4065276dddbc38e34c0ea8b2926fa8f7ff
sarinazeitooni <zeitoonisarina@gmail.com>
2021-07-22 12:40:14 +0430

    clean code

--- src/components/detailpage/comments/likes/LikeAndDislike.jsx
+++ src/components/detailpage/comments/likes/LikeAndDislike.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../style/like-and-dislike.scss';
 import {useDispatch, useSelector} from "react-redux";
 import ThumbDownOutlinedIcon from '@material-ui/icons/ThumbDownOutlined';
@@ -7,40 +7,33 @@ import actionTypes from "../../../../redux/actionTypes/actionTypes";
 function LikeAndDislike({data,id}){
     const dispatch = useDispatch();
     let items = useSelector((state) => state.likes.items);
-    let targetObject;
-    let target;
+    let targetObject , target;
     const [item , setItem] = useState(items.find((object) => object.id === id));
+    useEffect(()=>{
+        setItem(items.find((object) => object.id === id))
+    },[item])
     function Dispatch(action , targetItem){
         dispatch({
             type : action,
             targetItem : targetItem
         })
     }
-    function addDislike(id){
+    function add(id,action){
         target = data.find((object) => object.id === id);
         targetObject = {
             id : target.id,
             like : target.like,
             dislike : target.dislike
         }
-        Dispatch(actionTypes.Like , targetObject);
+        Dispatch(action , targetObject);
     }
-    function addLike(id){
-        target = data.find((object) => object.id === id);
-        targetObject = {
-            id : target.id,
-            like : target.like,
-            dislike : target.dislike
-        }
-        Dispatch(actionTypes.Dislike , targetObject);
-   }
     return(
         <div className='like-dislike-container'>
-            <button className='like-buttons' onClick={()=>{addDislike(id)}}>
+            <button className='like-buttons' onClick={()=>{add(id,actionTypes.Like)}}>
                 <ThumbUpAltOutlinedIcon/>
                 <span className='counter-container'>{item && item.like}</span>
             </button>
-            <button className='like-buttons' onClick={()=>{addLike(id)}}>
+            <button className='like-buttons' onClick={()=>{add(id,actionTypes.Dislike)}}>
                 <ThumbDownOutlinedIcon/>
                 <span className='counter-container'>{item && item.dislike}</span>
             </button>
