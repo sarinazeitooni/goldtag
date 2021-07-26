aad1db2613799a3d50f7ce64121032c02fd3db60
sarinazeitooni <zeitoonisarina@gmail.com>
2021-07-26 15:22:09 +0430

    added icon

--- src/redux/reducer/__tests__/comments.test.js
+++ src/redux/reducer/__tests__/comments.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import comments from "../comments";
 describe("comments reducer component", () => {
+
     const container = shallow(<comments/>)
     it("it should render", () => {
         expect(container).toMatchSnapshot();
